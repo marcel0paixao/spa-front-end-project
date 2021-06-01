c1be544937e281f75fe3afdac06eb987b9bb38d7
marcel0paixao <74371070+marcel0paixao@users.noreply.github.com>
2021-06-01 17:23:36 -0300

    fixing css

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function setDate() {
-    document.getElementById('seven_date').style.background = '#fff9f9'
-    document.getElementById('seven_date').type = 'date'
-}
-function setTime() {
-    document.getElementById('seven_time').style.background = '#fff9f9'
-    document.getElementById('seven_time').type = 'time'
-}
